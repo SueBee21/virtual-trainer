04e5d0862b1652a069a492fc3fad6b6c9c57e1b1
SusanBeno <beno.susan@gmail.com>
2020-12-08 21:25:23 -0500

    adding server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mongojs = require("mongojs");
+
+// create
+
+// read
+
+// update
+
+app.listen(8080, () => {
+  console.log("App running on port 8080!");
+});
